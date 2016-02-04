98c2ec06063bf3d450e5726a39a02372c88ee839
KaJuan Johnson <kdjohnson23@oakland.edu>
2016-02-03 23:18:49 -0500

    Remove comments from file #1

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php
 
 require 'vendor/autoload.php';
-/*
-$app = new \Slim\App();
 
-//Get contianer
-$container = $app->getContainer();
-
-
-//Register component on container 
-$container['view'] = function($container) {
-    $view = new \Slim\Views\Twig('./', [
-	    'cache' => 'cache']);
-
-    $view->addExtension(new \Slim\View\TwigExtension( 
-		$container['router'],
-		$container['request']->getUri()
-		));
-
-    return $view;
-};
-
-$app->get('/', function ($request, $response, $args) {
-	return $this->view->render($response, 'index.html');
-    });
-
-$app->run();*/
 // Create and configure Slim app
 $app = new \Slim\App;
 
