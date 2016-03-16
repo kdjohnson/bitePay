b0edd59d3f8589fffba7549120aca4ca76fca2da
KaJuan Johnson <kdjohnson23@oakland.edu>
2016-03-15 22:57:52 -0400

    Places html, css, js. Change to homepage to #11

--- index.php
+++ index.php
@@ -23,8 +23,16 @@ $container['view'] = function($container) {
 
 // Define app routes
 $app->get('/', function ($request, $response) {
-        return $this->view->render($response, 'index.html');
+	return $this->view->render($response, 'index.html');
+    return $this->view->render($response, 'index.html');
 });
 
+$app->get('/login', function ($request, $response) {
+    return $this->view->render($response, 'login.html');
+});
+
+$app->get('/places', function ($request, $response) {
+    return $this->view->render($response, 'places.html');
+});
 // Run app
 $app->run();
